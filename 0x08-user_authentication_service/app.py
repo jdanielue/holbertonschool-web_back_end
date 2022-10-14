070d4093ebb1a86aec34a88b0b60a1c06ee113f1
jdanielue <jdurregoe@gmail.com>
2022-10-14 07:39:43 -0500

    app 2

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Lorem ipsum dolor sit amet"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """ Base route for Lorem ipsum dolor sit amet """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Lorem ipsum dolor sit amet"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
