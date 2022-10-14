f16612a0f228070f9c7c3d8109daffc3154845aa
jdanielue <jdurregoe@gmail.com>
2022-10-14 07:11:57 -0500

    0x01x4

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
-"""Class User for ORM"""
+"""Class orm rep"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,7 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Representation of a user """
+    """Rep of a user """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
