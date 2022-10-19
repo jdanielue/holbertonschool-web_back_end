855a6119619850596c55e1124723e5aac25e8c64
jdanielue <jdurregoe@gmail.com>
2022-10-18 21:47:36 -0500

    0x02

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -19,4 +19,35 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test Lorem ipsum dolor sit amet """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+class TestMemoize(unittest.TestCase):
+    """ Lorem ipsum dolor sit amet """
+
+    def test_memoize(self):
+        """ Tlore,
+        """
+
+        class TestClass:
+            """ Test lorem """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
