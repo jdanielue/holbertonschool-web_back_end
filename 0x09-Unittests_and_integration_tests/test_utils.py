64244ee56d35374669fc3db3d8a8be2926a6add5
jdanielue <jdurregoe@gmail.com>
2022-10-18 21:48:23 -0500

    0x03

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,24 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
 
+
+class TestGetJson(unittest.TestCase):
+    """ Lorem ipsum dolor sit ametn """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Lorem ipsum dolor sit amet"""
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+
 class TestMemoize(unittest.TestCase):
     """ Lorem ipsum dolor sit amet """
 
