25589a54472d9895df0aba5ce12f08a88d67f245
jdanielue <jdurregoe@gmail.com>
2022-11-20 22:53:59 -0500

    from 4 to 7

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+
+
+  // moneda
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
+    // dinero
+    get amount() {
+      return this._amount;
+    }
+  
+    set amount(amount) {
+      if (typeof amount === 'number') this._amount = amount;
+    }
+}
+
+export default Pricing;
