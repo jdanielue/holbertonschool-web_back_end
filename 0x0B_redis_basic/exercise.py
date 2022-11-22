c93769ef86ec784dcba3870d8220794fdc1ddd69
jdanielue <jdurregoe@gmail.com>
2022-11-22 08:26:33 -0500

    exer 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -35,3 +35,72 @@ def call_history(method: Callable) -> Callable:
         return output
 
     return wrapper
+
+
+def replay(fn: Callable):
+    """Lorem ipsum dolor sit amet """
+    r = redis.Redis()
+    f_name = fn.__qualname__
+    n_calls = r.get(f_name)
+    try:
+        n_calls = n_calls.decode('utf-8')
+    except Exception:
+        n_calls = 0
+    print(f'{f_name} was called {n_calls} times:')
+
+    ins = r.lrange(f_name + ":inputs", 0, -1)
+    outs = r.lrange(f_name + ":outputs", 0, -1)
+
+    for i, o in zip(ins, outs):
+        try:
+            i = i.decode('utf-8')
+        except Exception:
+            i = ""
+        try:
+            o = o.decode('utf-8')
+        except Exception:
+            o = ""
+
+        print(f'{f_name}(*{i}) -> {o}')
+
+
+class Cache:
+    """ Lorem ipsum dolor sit amet  """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """SLorem ipsum dolor sit amet """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Lorem ipsum dolor sit amet  """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ PLorem ipsum dolor sit amet  """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Lorem ipsum dolor sit amet t """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
+
